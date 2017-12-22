b5d4a16f4d86a4838488fef307eea42543507477
Charles <regalferg@gmail.com>
2017-12-22 12:15:25 -0500

    Added functionality to add data to Events Panel from Ticketmaster API

--- assets/javascript/testEvent.js
+++ assets/javascript/testEvent.js
@@ -1,26 +1,29 @@
-var artist = '';
+var event = '';
  
  $("#add-band").on("click", function(event) {
-  artist = $("#band-input").val().trim();
+  event = $("#band-input").val().trim();
   var queryURL = 
-  "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Y68sacNOAQxxvGbr0Du9KNZNykWVrE3m&keyword=" + artist;
+  "https://app.ticketmaster.com/discovery/v2/events?apikey=Y68sacNOAQxxvGbr0Du9KNZNykWVrE3m&keyword=" + event;
    
       $.ajax({
           url: queryURL,
           method: "GET"
-        })
-       
-        .done(function(response) {
-          console.log(response)
+        }).done(function(response) {
+          console.log(response._embedded);
           document.preventDefault;
+
+
+          var results = response._embedded.events[0]._embedded.venues;
+          for (var i = 0; i < results.length; i++) {
+              var events = results[i];
+              console.log(events);
+              $("#band-display").html("<tr><td>" + events.name + "</td></tr>"); 
+              $("#band-display").append("<tr><td>" + events.address.line1 + "</td></tr>");    
+            }
+
           
           });
 
-          var results = response.events;
-          for (var i = 0; i < results.length; i++) {
-              var events = results[i].venue;
-              console.log(events);
-              $("#band-display").html("<tr><td>" + events + "</td></tr>");     
-            }
+          
 
     });
